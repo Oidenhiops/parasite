e299d9f6cd974dc3e69c9f9841b16cc526b6fed9
Oidenhiops <juanzateorozco@gmail.com>
2025-03-05 01:12:32 -0500

    Se crea prefab del reloj y su funcionalidad

---
+++ Assets/Scripts/Clock/ManagementClock.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ManagementClock : MonoBehaviour
+{
+    public float currentTimeToAppear;
+    public float timeToAppear;
+    public GameObject clock;
+    void Start()
+    {
+        currentTimeToAppear = timeToAppear;
+    }
+    void Update()
+    {
+        if (!clock.activeSelf)
+        {
+            currentTimeToAppear -= Time.deltaTime;
+            if (currentTimeToAppear <= 0)
+            {
+                currentTimeToAppear = timeToAppear;
+                clock.SetActive(true);
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            clock.SetActive(false);
+        }
+    }
+}
